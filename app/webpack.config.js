0f2f8823b937072f85dffdd4c20d13fed3094824
Asis Pattisahusiwa <79239132+asispts@users.noreply.github.com>
2023-02-08 09:11:03 +0900

    Add demo v1.4.3

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -16,14 +16,17 @@ Encore
         "jQuery.tagsinput": "bootstrap-tagsinput"
     })
     .enableSassLoader()
-    .enableVersioning(Encore.isProduction())
+    // when versioning is enabled, each filename will include a hash that changes
+    // whenever the contents of that file change. This allows you to use aggressive
+    // caching strategies. Use Encore.isProduction() to enable it only for production.
+    .enableVersioning(false)
     .addEntry('app', './assets/js/app.js')
     .addEntry('login', './assets/js/login.js')
     .addEntry('admin', './assets/js/admin.js')
     .addEntry('search', './assets/js/search.js')
     .splitEntryChunks()
     .enableSingleRuntimeChunk()
-    .enableIntegrityHashes()
+    .enableIntegrityHashes(Encore.isProduction())
     .configureBabel(null, {
         useBuiltIns: 'usage',
         corejs: 3,
